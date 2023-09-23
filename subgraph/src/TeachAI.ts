aaffa679d35319665cfadae586a418df15ee7ac8
denver <denverbaumgartner@gmail.com>
2023-09-23 00:45:22 -0400

    subgraph: mappings

--- subgraph/src/TeachAI.ts
+++ subgraph/src/TeachAI.ts
@@ -1,7 +1,7 @@
-import { ZERO_BI } from "../src/utils/constants";
+import { ZERO_BI, ONE_BI } from "../src/utils/constants";
 import { getUser } from "../src/utils/entities/user";
 import { getTransaction } from "../src/utils/entities/transaction";
-import { User, Transaction, Model, Prompt, Inference } from "../../../generated/schema";
+import { User, Transaction, Model, Prompt, Inference, Feedback } from "../../../generated/schema";
 import { logModel, logPromp, logInference, logFeedback } from '../../generated/TeachAI/TeachAI';
 
 export function handleLogModel(event: logModel): void {
@@ -29,6 +29,7 @@ export function handleLogModel(event: logModel): void {
         model.eventIndex = event.logIndex
         model.title = event.params.title
         model.owner = user.id
+        model.promptCount = ZERO_BI
         model.inferenceCount = ZERO_BI
         model.feedbackCount = ZERO_BI
         model.save()
@@ -46,8 +47,8 @@ export function handleLogModel(event: logModel): void {
 export function handleLogPrompt(event: logPromp): void {
     /**
      * logPrompt (
-     * string indexed prompt, # String of the prompt
      * bytes indexed model, # IPFS hash of the model
+     * string indexed prompt, # String of the prompt
      * )
      */
 
@@ -55,8 +56,52 @@ export function handleLogPrompt(event: logPromp): void {
     let model = Model.load(event.params.model)
 
     if (!model) {
-        // gracefully fail by creating 
+        // TODO: gracefully fail by creating a logModel event and passing to handleLogModel
+        return 
     }
+
+    // load the transaction entity
+    let transaction = getTransaction(event)
+
+    // load the user entity
+    let user = getUser(event.transaction.from)
+
+    // attempt to load the prompt entity
+    let prompt = Prompt.load(event.transaction.hash)
+
+    if (!prompt) {
+        // create the prompt entity
+        prompt = new Prompt(event.transaction.hash)
+        prompt.transaction = transaction.id
+        prompt.eventIndex = event.logIndex
+        prompt.model = model.id
+        prompt.user = user.id
+        prompt.text = event.params.prompt
+        prompt.save()
+
+        // update the model entity
+        model.promptCount = model.promptCount.plus(ONE_BI)
+        model.save()
+    } else {
+        prompt.transaction = transaction.id
+        prompt.eventIndex = event.logIndex
+        prompt.model = model.id
+        prompt.user = user.id
+        prompt.text = event.params.prompt
+        prompt.save()
+    }
+
+    return
+}
+
+export function handleLogInference(event: logInference): void {
+    /**
+     * logInference (
+     * string indexed prompt, # Transaction hash of the prompt
+     * string indexed textOne, # String of the first response
+     * string indexed textTwo, # String of the second response
+     * )
+     */
 
     // load the transaction entity
     let transaction = getTransaction(event)
@@ -68,43 +113,53 @@ export function handleLogPrompt(event: logPromp): void {
     let prompt = Prompt.load(event.params.prompt)
 
     if (!prompt) {
-        // create the prompt entity
-        prompt = new Prompt(event.params.prompt)
-        prompt.transaction = transaction.id
-        prompt.eventIndex = event.logIndex
-        prompt.text = event.params.prompt
-        prompt.owner = user.id
-        prompt.inferenceCount = ZERO_BI
-        prompt.feedbackCount = ZERO_BI
-        prompt.save()
-    } else {
-        prompt.transaction = transaction.id
-        prompt.eventIndex = event.logIndex
-        prompt.text = event.params.prompt
-        prompt.owner = user.id
-        prompt.save()
+        // TODO: gracefully fail by creating a logPrompt event and passing to handleLogPrompt
+        return 
     }
 
+    // attempt to load the model entity
+    let model = Model.load(prompt.model)
+
+    if (!model) {
+        // TODO: gracefully fail by creating a logModel event and passing to handleLogModel
+        return 
+    } 
+
+    // attempt to load the inference entity
+    let inference = Inference.load(event.transaction.hash)
+
+    if (!inference) {
+        // create the inference entity
+        inference = new Inference(event.transaction.hash)
+        inference.transaction = transaction.id
+        inference.eventIndex = event.logIndex
+        inference.model = model.id
+        inference.prompt = prompt.id
+        inference.user = user.id
+        inference.textOne = event.params.textOne
+        inference.textTwo = event.params.textTwo
+        inference.save()
+
+        // update the model entity
+        model.inferenceCount = model.inferenceCount.plus(ONE_BI)
+        model.save()
+    } else {
+        inference.transaction = transaction.id
+        inference.eventIndex = event.logIndex
+        inference.model = model.id
+        inference.prompt = prompt.id
+        inference.user = user.id
+        inference.textOne = event.params.textOne
+        inference.textTwo = event.params.textTwo
+        inference.save()
+    }
+    prompt.inference = inference.id
+    prompt.save()
+
     return
-
-}
-
-export function handleLogInference(event: logInference): void {
-
-    /**
-     * logInference (
-     * string indexed prompt, # Transaction hash of the prompt
-     * string indexed textOne, # String of the first response
-     * string indexed textTwo, # String of the second response
-     * )
-     */
-
-    return
-
 }
 
 export function handleLogFeedback(event: logFeedback): void {
-
     /**
      * logFeedback (
      * string indexed inference, # Transaction hash of the inference
@@ -112,40 +167,72 @@ export function handleLogFeedback(event: logFeedback): void {
      * )
      */
 
-    return
-
-}
-
-export function handleOffer(event: emitOffer): void {
-    
-    // get the transaction entity
+    // load the transaction entity
     let transaction = getTransaction(event)
 
-    // load the maker and from entities (users)
-    let maker = getUser(event.params.maker)
-    let from = event.transaction.from == event.params.maker ? maker : getUser(event.transaction.from)
+    // load the user entity
+    let user = getUser(event.transaction.from)
 
-    // load the pair entity
-    let pair = getPair(event.params.pay_gem, event.params.buy_gem)
+    // attempt to load the inference entity
+    let inference = Inference.load(event.params.inference)
 
-    // calculate the price of the offer (pay_amt / buy_amt)
-    let price = event.params.pay_amt.toBigDecimal().div(event.params.buy_amt.toBigDecimal())
+    if (!inference) {
+        // TODO: gracefully fail by creating a logInference event and passing to handleLogInference
+        return 
+    }
 
-    // make the offer entity
-    let offer = new Offer(event.params.id)
-    offer.transaction = transaction.id
-    offer.timestamp = event.block.timestamp
-    offer.index = event.logIndex
-    offer.maker = maker.id
-    offer.from_address = from.id
-    offer.pair = pair.id
-    offer.pay_gem = event.params.pay_gem
-    offer.buy_gem = event.params.buy_gem
-    offer.pay_amt = event.params.pay_amt
-    offer.buy_amt = event.params.buy_amt
-    offer.paid_amt = ZERO_BI
-    offer.bought_amt = ZERO_BI
-    offer.price = price
-    offer.open = true
-    offer.save()
+    // attempt to load the model entity
+    let model = Model.load(inference.model)
+
+    if (!model) {
+        // TODO: gracefully fail by creating a logModel event and passing to handleLogModel
+        return 
+    }
+
+    // attempt to load the prompt entity
+    let prompt = Prompt.load(inference.prompt)
+
+    if (!prompt) {
+        // TODO: gracefully fail by creating a logPrompt event and passing to handleLogPrompt
+        return 
+    }
+
+    // attempt to load the feedback entity
+    let feedback = Feedback.load(event.transaction.hash)
+
+    if (!feedback) {
+        // create the feedback entity
+        feedback = new Feedback(event.transaction.hash)
+        feedback.transaction = transaction.id
+        feedback.eventIndex = event.logIndex
+        feedback.model = model.id
+        feedback.user = user.id
+        feedback.prompt = prompt.id
+        feedback.inference = inference.id
+        feedback.isOneBetter = event.params.isOneBetter
+        feedback.save()
+
+        // update the model entity
+        model.feedbackCount = model.feedbackCount.plus(ONE_BI)
+        model.save()
+
+        // update the prompt entity
+        prompt.feedback = feedback.id
+        prompt.save()
+    } else {
+        feedback.transaction = transaction.id
+        feedback.eventIndex = event.logIndex
+        feedback.model = model.id
+        feedback.user = user.id
+        feedback.prompt = prompt.id
+        feedback.inference = inference.id
+        feedback.isOneBetter = event.params.isOneBetter
+        feedback.save()
+
+        // update the prompt entity
+        prompt.feedback = feedback.id
+        prompt.save()
+    }
+
+    return
 }
